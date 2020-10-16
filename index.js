0e0c01eb1b12c82d19a01bc52d30765b4b7eca17
ardan <ardan@localhost.localdomain>
2020-10-16 15:21:34 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-  console.log('Server dijalankan');
+router.use('/products', (req, res, next) => {
+  res.json({ name: "Ardan Juniar", email: "ardanjuniar@gmail.com" });
+  next();
 })
+
+router.use('/price', (req, res, next) => {
+  res.json({ name: "Sweeter", price: 100000 });
+  next();
+})
+
+router.get('/customers', (req, res, next) => {
+  res.json({ name: "Muhammad Tachril", email: "muhammadtachril@gmail.com" });
+  next();
+})
+
+app.use('/', router);
 
 app.listen(4000);

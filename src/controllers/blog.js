e658f61579a2d378c71999af2f1650551317d4e0
ardan <ardan@localhost.localdomain>
2020-10-25 21:07:35 -0400

    Membuat Fungsi getAllBlogPost Membuat Fungsi getBlogPostById Membuat Fungsi updateBlogPost Membuat Fungsi deleteBlogPost dan removeImage

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,11 +1,14 @@
 const { validationResult } = require('express-validator');
 const BlogPost = require('../models/blog');
+const { Error } = require('mongoose');
+const path = require('path');
+const fs = require('fs');
 
 exports.createBlogPost = (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        const err = new Error('Invalid Value Tidak Sesuai');
+        const err = new Error('Input Value Tidak Sesuai');
         err.errorStatus = 400;
         err.data = errors.array();
         throw err;
@@ -38,4 +41,117 @@ exports.createBlogPost = (req, res, next) => {
         .catch(err => {
             console.log('Err: ', err);
         });
+}
+
+exports.getAllBlogPost = (req, res, next) => {
+    BlogPost.find()
+        .then(result => {
+            res.status(200).json({
+                message: 'Data blog post berhasil dipanggil',
+                data: result
+            })
+        })
+        .catch(err => {
+            next(err)
+        });
+}
+
+exports.getBlogPostById = (req, res, next) => {
+    const postId = req.params.postId;
+    BlogPost.findById(postId)
+        .then(result => {
+            if (!result) {
+                const error = new Error('Blog post tidak ditemukan');
+                error.errorStatus = 404;
+                throw error;
+            }
+
+            res.status(200).json({
+                message: 'Data blog post berhasil dipanggil',
+                data: result,
+            });
+        })
+        .catch(err => {
+            next(err);
+        });
+}
+
+exports.updateBlogPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const err = new Error('Input Value Tidak Sesuai');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+
+    if (!req.file) {
+        const err = new Error('Image harus di upload');
+        err.errorStatus = 422;
+        throw err;
+    }
+
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const body = req.body.body;
+    const image = req.file.path;
+
+    BlogPost.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Blog post tidak ditemukan');
+                error.errorStatus = 404;
+                throw error;
+            }
+
+            post.title = title;
+            post.body = body;
+            post.image = image;
+
+            return post.save();
+        })
+        .then(result => {
+            res.status(200).json({
+                message: 'Update sukses',
+                data: result,
+            })
+        })
+        .catch(err => {
+            next(err);
+        });
+
+}
+
+exports.deleteBlogPost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+        .then(post => {
+            if (!post) {
+                const err = new Error('Input Value Tidak Sesuai');
+                err.errorStatus = 400;
+                err.data = errors.array();
+                throw err;
+            }
+
+            removeImage(post.image);
+            return BlogPost.findByIdAndRemove(postId);
+        })
+        .then(result => {
+            res.status(200).json({
+                message: 'Hapus blog post berhasil',
+                data: result,
+            });
+        })
+        .catch(err => {
+            next(err);
+        });
+}
+
+const removeImage = (filePath) => {
+    filePath = path.join(__dirname, '../..', filePath);
+    fs.unlink(filePath, err => {
+        console.log(err)
+    });
 }
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -9,5 +9,13 @@ router.post('/post', [
     body('body').isLength({ min: 5 }).withMessage('Input body tidak sesuai')],
     blogController.createBlogPost
 );
+router.get('/posts', blogController.getAllBlogPost);
+router.get('/post/:postId', blogController.getBlogPostById);
+router.put('/post/:postId', [
+    body('title').isLength({ min: 5 }).withMessage('Input title tidak sesuai'),
+    body('body').isLength({ min: 5 }).withMessage('Input body tidak sesuai')],
+    blogController.updateBlogPost
+);
+router.delete('/post/:postId', blogController.deleteBlogPost);
 
 module.exports = router
